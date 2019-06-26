c25b8e5c750a3036eb7763b79d0aa21b9f9105c9
Christian Perez <perezchristian1012@gmail.com>
2019-06-26 02:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function nowServing(katzDeliLine) {
 
   }
 }
-function currentLine(katzDeliLine) {
+function currentLine(line) {
   if (katzDeliLine.length === 0) {
     return 'The line is currently empty.';
   } else {
-    for (let i =0; i < katzDeliLine.length; i++) {
-       katzDeli.push(` ${i + 1}. ${katzDeliLine[i]}`)
+    for (let i =0; i < line.length; i++) {
+       katzDeli.push(` ${i + 1}. ${line[i]}`)
      }
    }
-   return `The line is currently:${katzDeli}`
+   return `The line is currently:${line}`
  }
